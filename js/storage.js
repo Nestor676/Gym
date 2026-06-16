72840f12347f6205508f2248f2812334633b4fde
Nestor676 <nestorschierse@paucasesnovescifp.cat>
2026-06-16 12:52:24 +0200

    JS: DeleteSessio creado + getReserves y saveReserves

--- js/storage.js
+++ js/storage.js
@@ -4,6 +4,7 @@ import { Sessio, Reserva } from './models.js';
 const SESSIONS_KEY = 'gimnas_sessions';
 
 /* Sessions */
+
 function getSessions() {
 
   const res = localStorage.getItem(SESSIONS_KEY);
@@ -23,4 +24,30 @@ function addSessio(sessio) {
   saveSessions(sessions);
 }
 
-export { getSessions, saveSessions, addSessio };
+function deleteSessio(id) {
+
+  const sessions = getSessions().filter(s => s.id !== id);
+  saveSessions(sessions);
+
+  const reserves = getReserves().filter(r => r.sessioId !== id);
+  saveReserves(reserves);
+}
+
+/* Reserves */
+
+const RESERVES_KEY = 'gimnas_reserves';
+
+function getReserves() {
+
+  const res = localStorage.getItem(RESERVES_KEY);
+
+  if (!res) return [];
+
+  return JSON.parse(res).map(r => new Reserva(r));
+}
+
+function saveReserves(reserves) {
+  localStorage.setItem(RESERVES_KEY, JSON.stringify(reserves));
+}
+
+export { getSessions, saveSessions, addSessio, deleteSessio, getReserves, saveReserves };
