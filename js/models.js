bcba24098d693f884321963ee5103e8d262ec4e7
Nestor676 <nestorschierse@paucasesnovescifp.cat>
2026-06-16 11:30:54 +0200

    JS: Clase Sessio creada

---
+++ js/models.js
@@ -0,0 +1,13 @@
+
+class Sessio {
+  constructor({ id, activitat, dia, hora, monitor, aforament }) {
+    this.id = id;
+    this.activitat = activitat;
+    this.dia = dia;
+    this.hora = hora;
+    this.monitor = monitor;
+    this.aforament = aforament;
+  }
+}
+
+export { Sessio };
